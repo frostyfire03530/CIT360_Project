c98c4eb53513fa94c9bb37bbbb0e34c3651731ed
Josh Brown <josh2brown@gmail.com>
2014-05-30 12:30:45 -0600

    updated server.java

--- sandbox/server_client/javafun/src/javafun/server.java
+++ sandbox/server_client/javafun/src/javafun/server.java
@@ -3,6 +3,8 @@ package javafun;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.swing.JOptionPane;
+
 import org.quickconnectfamily.json.JSONInputStream;
 import org.quickconnectfamily.json.JSONOutputStream;
 
@@ -10,18 +12,28 @@ import org.quickconnectfamily.json.JSONOutputStream;
 public class server {
 
 	public static void main (String[] args){
-		try{
-		ServerSocket aListeningSocket = new ServerSocket(9292);
-		Socket clientSocket = aListeningSocket.accept();
+		while (true) {
 
-		JSONInputStream inFromClient = 
-				new JSONInputStream(clientSocket.getInputStream());
-		JSONOutputStream outToClient = 
-				new JSONOutputStream(clientSocket.getOutputStream());
+
+			try{
+				ServerSocket aListeningSocket = new ServerSocket(80);
+				Socket clientSocket = aListeningSocket.accept();
+
+				JSONInputStream inFromClient = 
+						new JSONInputStream(clientSocket.getInputStream());
+				JSONOutputStream outToClient = 
+						new JSONOutputStream(clientSocket.getOutputStream());
+
+				String message = (String) inFromClient.readObject();
+				//JOptionPane.showMessageDialog (null,"Your password is "+ message );
+				String comp = "Server Says: Your data is " + "\"" + message + "\"";
+				outToClient.writeObject(comp);
+			}
+			catch(Exception e){
+
+				//e.printStackTrace();
+			}
 		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		}
+	}
 
 }
